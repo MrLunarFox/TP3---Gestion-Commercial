0397c1568e9ca374950755ea90c2dd6d1b35595b
HIS <hisjosuapro@gmail.com>
2023-02-15 14:09:41 +0100

    Ajoutez des fichiers projet.

---
+++ TP3 - Gestion Commercial/Commercial.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Serialization;
+
+namespace TP3___Gestion_Commercial
+{
+    internal class Commercial
+    {
+        private string nom;
+        private string prenom;
+        private int anneeNaiss;
+
+        public string Nom { get => nom; set => nom = value; }
+        public string Prenom { get => prenom; set => prenom = value; }
+        public int AnneeNaiss { get => anneeNaiss; set => anneeNaiss = value; }
+
+        public Commercial(string nomC, string prenomC, int anneeNaissC)
+        {
+            this.nom = nomC;
+            this.prenom = prenomC;
+            this.anneeNaiss = anneeNaissC;
+        }
+
+        public Commercial()
+        {
+
+        }
+
+        public int calculAge()
+        {
+            int difNaiss = DateTime.Now.Year - this.AnneeNaiss;
+            return difNaiss;
+        }
+
+        public override string ToString()
+        {
+            return "nom : " + Nom + "\n" + 
+                   "prenom : " + Prenom + "\n" + 
+                   "Année de naissance :" + AnneeNaiss.ToString() + "\n" + 
+                   "Ce commercial a donc " + calculAge() + " ans.";
+        }
+
+        /*
+        public void compare(Commercial c)
+        {
+            if ()
+            {
+
+            }
+        }
+        */
+    }
+}
---
+++ TP3 - Gestion Commercial/Program.cs
@@ -0,0 +1,9 @@
+﻿using TP3___Gestion_Commercial;
+
+Commercial c1 = new Commercial("toto", "A", 2004);
+Commercial c2 = new Commercial("titi", "B", 1996);
+Commercial c3 = new Commercial("tata", "C", 1999);
+
+Console.WriteLine(c1.ToString());
+Console.WriteLine(c2.ToString());
+Console.WriteLine(c3.ToString());
